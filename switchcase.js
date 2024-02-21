6c3c262322d9b7df2ab5dd1f1044f95239ad424f
JonesJonh <143019996+JonesJonh@users.noreply.github.com>
2024-02-21 12:17:41 -0300

    Add files via upload

---
+++ switchcase.js
@@ -0,0 +1,46 @@
+const sortear=() => { 
+    let nomes = ["ROBESVALDO 👍","PINK FLOID🎸","MACARENA💃🏻","MICHAEL JACKSON🕴","WINKS 🧚‍♀️","PATRULHA CANINA 🐶","BARBIE👸","KEN 👱🏻‍♂️","MONSTER HIGH 🧟‍♀️"]
+    
+    let h1Nome = document.getElementById("nome");
+    
+    let achouNumero = false;
+    let numeroSorteado;
+    
+    while(achouNumero == false) {
+        numeroSorteado = Math.floor(Math.random() * 100);
+        if (numeroSorteado < nomes.length) {
+            achouNumero = true;
+        }
+    }
+
+    switch (numeroSorteado) {
+     case 0:
+        h1Nome.innerText = nomes[numeroSorteado];
+        break;
+    case 1:
+        h1Nome.innerText = nomes[numeroSorteado];
+        break;
+    case 2:
+        h1Nome.innerText = nomes[numeroSorteado];
+        break;
+    case 3:
+        h1Nome.innerText = nomes[numeroSorteado];
+        break;
+    case 4:
+        h1Nome.innerText = nomes[numeroSorteado];
+        break;
+    case 5:
+        h1Nome.innerText = nomes[numeroSorteado];
+        break;
+    case 6:
+        h1Nome.innerText = nomes[numeroSorteado];
+        break;
+    case 7:
+        h1Nome.innerText = nomes[numeroSorteado];
+        break;
+    case 8:
+        h1Nome.innerText = nomes[numeroSorteado];
+        break;
+        default:
+        }
+}
